180dab16b01a683db014531c3884197018df2cad
Marcos <marsanmar6291@gmail.com>
2023-03-24 16:56:19 -0600

    correcion mail

--- components/footer.js
+++ components/footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
 
       </div>
   
-      <h3>Contacto: info@minerianews.com</h3>
+      <h3>Contacto: jhernandez.dwi@gmail.com</h3>
       <p>Mineria news es un medio independiente diseñado para tener las noticias mas importantes y actuales sobre la minera en México y en el mundo.</p>
 
     </footer>
