814a0a942f842c49f9c1a145035116e5db958c62
Marcos <marsanmar6291@gmail.com>
2023-03-24 13:55:20 -0600

    Favicon imagenes

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet" />
       <link rel='stylsheet' href='https://necolas.github.io/normalize.css/8.0.1/normalize.css' />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
     <body>
       <Main />
