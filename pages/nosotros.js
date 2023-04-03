ed3512a7f90d6edf1d38bd15a96e7e692ffe37e5
Marcos <marsanmar6291@gmail.com>
2023-04-03 10:42:18 -0600

    Nueva seccion de nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout";
 import styles from '@/styles/nosotros.module.css';
 
-export default function Noticias({proveedores}) {
+export default function Nosotros() {
 
 
   return (
