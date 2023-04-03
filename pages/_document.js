0fa0819ec1e1e0361ff3e04e364d1e78f5f6b402
Marcos <marsanmar6291@gmail.com>
2023-04-03 16:02:24 -0600

    Cambios en document google analytcs tags

--- pages/_document.js
+++ pages/_document.js
@@ -21,18 +21,16 @@ export default function Document() {
     <Script  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-8YF6K6NR9F" />
     <Script 
       id='google-analytics'
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-8YF6K6NR9F', {
-                  page_path: window.location.pathname,
-                });
-              `,
-              }}
-          />
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-8YF6K6NR9F');
+      `,
+      }}
+      />
     <body>
       <Main />
       <NextScript />
