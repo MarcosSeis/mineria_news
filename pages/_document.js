c6d6202456c0a262772e2e890b96b3298ec99216
Marcos <marsanmar6291@gmail.com>
2023-04-03 11:20:54 -0600

    Cambios en document google analytcs tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -17,6 +18,21 @@ export default function Document() {
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
     </Head>
+    <Script  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-8YF6K6NR9F" />
+    <Script 
+      id='google-analytics'
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-8YF6K6NR9F', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+          />
     <body>
       <Main />
       <NextScript />
