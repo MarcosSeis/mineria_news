c56e06f3ee585597b21dbf9c2e686387e3696770
Marcos <marsanmar6291@gmail.com>
2023-04-10 09:43:04 -0600

    cambio de header

--- pages/_document.js
+++ pages/_document.js
@@ -7,34 +7,17 @@ export default function Document() {
     <Head>
       <meta name="description" content="Mineria news- Noticias sobre mineria, geologia, ciencias de la tierra, metalurgia" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet" />
       <link rel='stylsheet' href='https://necolas.github.io/normalize.css/8.0.1/normalize.css' />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
-    </Head>
-     
-
-    <Script
-      src="https://www.googletagmanager.com/gtag/js?id=G-8YF6K6NR9F "
-      strategy="afterInteractive"
-    />
-    <Script id="google-analytics" strategy="afterInteractive">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){window.dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-8YF6K6NR9F ');
-      `}
-    </Script>
-
-   
+    </Head>   
     <body>
       <Main />
       <NextScript />
